b92cb8cb3fe1f6b4e834dfa49fe42b71082abfea
a12989x <alexis12989x@gmail.com>
2020-10-17 11:10:54 -0500

    Set up Product Component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
-const Product = () => {
+const Product = ({ id, title, price, rating, imgUrl }) => {
     return (
-        <div>
-            <p>Product Component</p>
+        <div className='product'>
+            <div className='product__info'>
+                <p>{title}</p>
+                <p className='product__price'>
+                    <small>&#36;</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className='product__rating'>
+                    <span role='img' aria-label='star'>
+                        {Array(rating)
+                            .fill()
+                            .map((_, i) => (
+                                <span
+                                    key={i}
+                                    aria-label='dollar sign'
+                                    role='img'
+                                >
+                                    &#11088;
+                                </span>
+                            ))}
+                    </span>
+                </div>
+            </div>
+
+            <img className='product__img' src={imgUrl} alt='product' />
+            <button className='product__btn'>Add to Basket</button>
         </div>
     );
 };
