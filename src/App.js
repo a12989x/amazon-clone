a13f531a932f5f14d7025b25f5c833e5f1b8a05a
a12989x <alexis12989x@gmail.com>
2020-10-17 11:13:17 -0500

    Add config for routes in app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Header from './components/Header';
-import Home from './components/Home';
+import Home from './components/pages/Home';
 
 function App() {
     return (
-        <div className="App">
-            <Header />
-            <Home />
+        <div className='App'>
+            <BrowserRouter>
+                <Header />
+                <Switch>
+                    <Route exact path='/' component={Home} />
+                </Switch>
+            </BrowserRouter>
         </div>
     );
 }
