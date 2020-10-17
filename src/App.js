5670288169d4b44e69d294453f31a078ee2b77ac
a12989x <alexis12989x@gmail.com>
2020-10-17 09:28:03 -0500

    Import Header and Home components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Header from './components/Header';
+import Home from './components/Home';
+
 function App() {
     return (
         <div className="App">
-            <h2>Amazon CLone</h2>
+            <Header />
+            <Home />
         </div>
     );
 }
