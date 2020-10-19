c29a6fb2a6be52cd5ab323851ecb43640c0162e4
a12989x <alexis12989x@gmail.com>
2020-10-19 11:14:32 -0500

    Set up Checkout component

--- src/components/pages/Checkout.js
+++ src/components/pages/Checkout.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Subtotal from '../Subtotal';
 
 const Checkout = () => {
     return (
-        <div>
-            <p>Checkout Component</p>
+        <div className='checkout'>
+            <div className='checkout__left'>
+                <Link to='/'>
+                    <img
+                        className='checkout__imgAd'
+                        src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg'
+                        alt=''
+                    />
+                </Link>
+
+                <h2 className='checkout__title'>Your shopping basket</h2>
+            </div>
+
+            <div className='checkout__right'>
+                <h3>The subtotal will go here</h3>
+                <Subtotal />
+            </div>
         </div>
     );
 };
