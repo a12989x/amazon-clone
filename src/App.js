8e9603f16483930fa4ba2c9ae465d89a25834f6f
a12989x <alexis12989x@gmail.com>
2020-10-19 09:41:05 -0500

    Import Checkout component and add /checkout route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Home from './components/pages/Home';
+import Checkout from './components/pages/Checkout';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Header />
                 <Switch>
                     <Route exact path='/' component={Home} />
+                    <Route exact path='/checkout' component={Checkout} />
                 </Switch>
             </BrowserRouter>
         </div>
