f446e1cf4638555eb48b4c84e568079514cdf410
a12989x <alexis12989x@gmail.com>
2020-10-19 17:58:23 -0500

    Import and add context

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { StateContextProvider } from './components/store/StateContext';
+import reducer, { initialState } from './components/store/reducer';
+
 import App from './App';
 
 import './scss/main.scss';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <StateContextProvider initialState={initialState} reducer={reducer}>
+            <App />
+        </StateContextProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
