d177fbd2019a5eb2e51711d1c42ef9a8d1315a0f
a12989x <alexis12989x@gmail.com>
2020-10-20 10:08:52 -0500

    Set up Subtotal component

--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import CurrencyFormat from 'react-currency-format';
+
+import { useStateValue } from './store/StateContext';
+import { getBasketTotal } from './store/reducer';
 
 const Subtotal = () => {
+    const [{ basket }] = useStateValue();
+
     return (
         <div className='subtotal'>
-            <p>Subtotal COmponent</p>
+            <CurrencyFormat
+                renderText={(value) => (
+                    <>
+                        <p>
+                            subtotal ({basket.length} items):
+                            <strong>{value}</strong>
+                        </p>
+                        <small className='subtotal__gift'>
+                            <input type='checkbox' />
+                            This order contains a gift
+                        </small>
+                    </>
+                )}
+                decimalScale={2}
+                value={getBasketTotal(basket)}
+                displayType={'text'}
+                thousandSeparator={true}
+                prefix={'$'}
+            />
+            <button className='subtotal__btn'>Proceed to checkout</button>
         </div>
     );
 };
