936fe73bb47c8e4f69a31cedbc807febe7039a5c
a12989x <alexis12989x@gmail.com>
2020-10-20 16:20:47 -0500

    Set up Header component with Links and dynamic basket

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,61 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { auth } from 'firebase';
+
+import { useStateValue } from './store/StateContext';
 
 const Header = () => {
-    return (
-        <header className="header">
-            <img
-                className="header__logo"
-                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-                alt="amazon logo"
-            />
+    const [{ basket, user }, dispatch] = useStateValue();
 
-            <div className="header__search">
-                <input type="text" className="header__searchInput" />
-                <SearchIcon className="header__searchIcon" />
+    const handleAuthentication = () => {
+        if (user) {
+            auth.signOut();
+        }
+    };
+
+    return (
+        <header className='header'>
+            <Link to='/'>
+                <img
+                    className='header__logo'
+                    src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
+                    alt='amazon logo'
+                />
+            </Link>
+
+            <div className='header__search'>
+                <input type='text' className='header__searchInput' />
+                <SearchIcon className='header__searchIcon' />
             </div>
 
-            <nav className="header__nav">
-                <div className="header__options">
-                    <span>Hello</span>
-                    <span>Sign In</span>
-                </div>
-                <div className="header__options">
+            <nav className='header__nav'>
+                <Link to={!user && '/sign-in'}>
+                    <div
+                        onClick={handleAuthentication}
+                        className='header__options'
+                    >
+                        <span>Hello</span>
+                        <span>Sign {user ? 'In' : 'Out'}</span>
+                    </div>
+                </Link>
+                <div className='header__options'>
                     <span>Returns</span>
                     <span>&#38; Orders</span>
                 </div>
-                <div className="header__options">
+                <div className='header__options'>
                     <span>Your</span>
                     <span>Prime</span>
                 </div>
-                <div className="header__optionBasket">
-                    <ShoppingBasketIcon />
-                    <span className="header__basketCount">0</span>
-                </div>
+                <Link to='/checkout'>
+                    <div className='header__optionBasket'>
+                        <ShoppingBasketIcon />
+                        <span className='header__basketCount'>
+                            {basket?.length}
+                        </span>
+                    </div>
+                </Link>
             </nav>
         </header>
     );
