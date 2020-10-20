6de674922e37be3a870157fa83cc0b43915022a0
a12989x <alexis12989x@gmail.com>
2020-10-20 16:22:10 -0500

    Add code to sign in and register functions

--- src/components/pages/SignIn.js
+++ src/components/pages/SignIn.js
@@ -1,16 +1,30 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+
+import { auth } from '../../firebase';
 
 const SignIn = () => {
+    const history = useHistory();
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const signIn = (e) => {
         e.preventDefault();
+
+        auth.signInWithEmailAndPassword(email, password)
+            .then((auth) => history.push('/'))
+            .catch((err) => alert(err.message));
     };
 
     const register = (e) => {
         e.preventDefault();
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .then((auth) => {
+                if (auth) history.push('/');
+            })
+            .catch((err) => alert(err.message));
     };
 
     return (
