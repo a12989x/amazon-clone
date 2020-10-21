5403a160e6ad36b4e9dedafe501aef044998e96a
a12989x <alexis12989x@gmail.com>
2020-10-21 12:19:03 -0500

    Add stripe config and with imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 
 import { auth } from './firebase';
 import { useStateValue } from './components/store/StateContext';
@@ -9,6 +11,10 @@ import Home from './components/pages/Home';
 import Checkout from './components/pages/Checkout';
 import SignIn from './components/pages/SignIn';
 import Payment from './components/pages/Payment';
+
+const promise = loadStripe(
+    'pk_test_51HeUaFHxPghHyDls1HUYzze4UDJfSlqoggKfhpw2oRVzoMmQtaZI1qU8tkgy44j6GvRQjEDmn4X0w11MLVfMKcSl00fmisJHtg'
+);
 
 function App() {
     const [{}, dispatch] = useStateValue();
@@ -34,7 +40,11 @@ function App() {
                     <Route exact path='/' component={Home} />
                     <Route exact path='/sign-in' component={SignIn} />
                     <Route exact path='/checkout' component={Checkout} />
-                    <Route exact path='/payment' component={Payment} />
+                    <Route exact path='/payment'>
+                        <Elements stripe={promise}>
+                            <Payment />
+                        </Elements>
+                    </Route>
                 </Switch>
             </BrowserRouter>
         </div>
