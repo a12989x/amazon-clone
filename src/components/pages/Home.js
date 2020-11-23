f45bf93907f5584606edc99332ecea749a76c0d3
a12989x <alexis12989x@gmail.com>
2020-11-23 09:55:10 -0600

    Delete ProductsData and Create randomNumbers functiion

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,64 +1,24 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+
+import { ProductsContext } from '../contexts/ProductsContext';
 
 import Product from '../Product';
 
-const productsData = [
-    {
-        id: '5cc75631-4631-434c-81ab-1057d7b5534b',
-        title:
-            'The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback',
-        price: 11.96,
-        rating: 5,
-        imgUrl:
-            'https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg',
-    },
-    {
-        id: '7119ba6a-b185-421c-845c-3de3defe9c42',
-        title:
-            'Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl',
-        price: 239.0,
-        rating: 4,
-        imgUrl:
-            'https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg',
-    },
-    {
-        id: '1a23e8eb-f9e4-40cb-b563-1006cc2464ef',
-        title: "Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor",
-        price: 199.99,
-        rating: 3,
-        imgUrl:
-            'https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg',
-    },
-    {
-        id: 'd02a78e3-9202-413a-964a-db379ef73469',
-        title:
-            'Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric',
-        price: 98.99,
-        rating: 5,
-        imgUrl:
-            'https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$',
-    },
-    {
-        id: '54c119ed-b7f5-4f9d-867b-9860243b81b5',
-        title:
-            'New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)',
-        price: 598.99,
-        rating: 4,
-        imgUrl:
-            'https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg',
-    },
-    {
-        id: 'd17db7a8-dd7e-4534-a9c0-20c95bd337a4',
-        title:
-            "Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440",
-        price: 1094.98,
-        rating: 4,
-        imgUrl:
-            'https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg',
-    },
-];
-
 const Home = () => {
+    const { products } = useContext(ProductsContext);
+
+    const [randomNumbers, setRandomNumbers] = useState([]);
+
+    useEffect(() => {
+        let nums = [];
+        for (let i = 0; i < products.length; i++) {
+            let randomNumber = Math.floor(Math.random() * products.length);
+
+            nums.push(randomNumber);
+        }
+        setRandomNumbers(nums);
+    }, [products]);
+
     return (
         <div className='home'>
             <div className='home__container'>
@@ -69,18 +29,18 @@ const Home = () => {
                 />
 
                 <div className='home__row'>
-                    <Product {...productsData[0]} />
-                    <Product {...productsData[1]} />
+                    <Product page='home' {...products[randomNumbers[0]]} />
+                    <Product page='home' {...products[randomNumbers[1]]} />
                 </div>
 
                 <div className='home__row'>
-                    <Product {...productsData[2]} />
-                    <Product {...productsData[3]} />
-                    <Product {...productsData[4]} />
+                    <Product page='home' {...products[randomNumbers[2]]} />
+                    <Product page='home' {...products[randomNumbers[3]]} />
+                    <Product page='home' {...products[randomNumbers[4]]} />
                 </div>
 
                 <div className='home__row'>
-                    <Product {...productsData[5]} />
+                    <Product page='home' {...products[randomNumbers[5]]} />
                 </div>
             </div>
         </div>
