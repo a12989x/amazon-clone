04eb2cfba32a4d79bc443f9ecf8bda9ee64742b6
a12989x <alexis12989x@gmail.com>
2020-11-23 10:36:29 -0600

    Set stripe.public_key variables and delete local variable

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,6 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const cors = require('cors');
-const stripe = require('stripe')(
-    'sk_test_51HeUaFHxPghHyDls15Ihw9gFZyjMoWvzgFvqAD2ey6pkM8fmAD5XZiPDSJHzQXM9WEKUo8bdlgzE0dkcvPyLm6Xr00MV1fGg87'
-);
 
 const app = express();
 
@@ -12,10 +9,12 @@ app.use(express.json());
 
 app.post('/payments/create', async (req, res) => {
     const total = req.query.total;
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: total,
-        currency: 'usd',
-    });
+    const paymentIntent = await functions
+        .config()
+        .stripe.public_key.paymentIntents.create({
+            amount: total,
+            currency: 'usd',
+        });
 
     res.status(201).send({ clientSecret: paymentIntent.client_secret });
 });
